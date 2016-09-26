0737cfc31e457f76ae4fc91ebe33cd5b7f1f9250
Michael Droettboom <mdboom@gmail.com>
2016-09-26 13:35:57 -0400

    Don't write test files to disk

--- tests/test_files.py
+++ tests/test_files.py
@@ -26,9 +26,6 @@ def test_files(filename):
         fd2 = io.BytesIO()
         f.write(fd2)
 
-        with open(filename + '.rt.psd', 'wb') as fd_disk:
-            f.write(fd_disk)
-
     fd2.seek(0)
     f = psdwriter.PsdFile.read(fd2)
 
