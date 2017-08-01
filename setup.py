f6677a54cee3e83e3b3f2e0ed82cb190731e5dad
Michael Droettboom <mdboom@gmail.com>
2017-08-01 14:43:05 -0400

    Fix bumpversion (hopefully for the last time)

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# disable interpolation in configparser
-import configparser
-configparser.ConfigParser = configparser.RawConfigParser
-
 
 from setuptools import setup
 from setuptools.extension import Extension
